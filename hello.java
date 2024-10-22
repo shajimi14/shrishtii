0219626c6e301ef01e0c8b159dad0ccf2f395043
shajimi <shajimips14@gmail.com>
2024-10-22 10:08:43 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int age=0;
+         int yob=2010;
+           int currentyear=2024;
+            age= currentyear-yob;
+             int x=18-age ;
+            if(age<18){
+                System.out.println("u are not elligible vote  u can vote after"+ x );
+            }
+            else{
+                System.out.println("elligible to vote");
+
+            }
+             
+	}
+}
